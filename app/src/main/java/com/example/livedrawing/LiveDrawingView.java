3379291cc15d5520fefec7a9cb29c9f4b65d384a
haschish <haschish@gmail.com>
2020-04-04 02:09:07 +0500

    step 1

---
+++ app/src/main/java/com/example/livedrawing/LiveDrawingView.java
@@ -0,0 +1,98 @@
+package com.example.livedrawing;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class LiveDrawingView extends SurfaceView implements Runnable {
+    private final boolean DEBUGGING = true;
+    private SurfaceHolder holder;
+    private Canvas canvas;
+    private Paint paint;
+
+    private long FPS;
+    private final int MILLIS_IN_SECONDS = 1000;
+
+    private int screenX;
+    private int screenY;
+    private int fontSize;
+    private int marginSize;
+
+    private Thread thread = null;
+    private volatile boolean drawing;
+    private boolean paused = true;
+
+    public LiveDrawingView(Context context, int x, int y) {
+        super(context);
+        screenX = x;
+        screenY = y;
+        fontSize = screenX / 20;
+        marginSize = screenX / 75;
+        holder = getHolder();
+        paint = new Paint();
+    }
+
+    private void draw() {
+        if (holder.getSurface().isValid()) {
+            canvas = holder.lockCanvas();
+            canvas.drawColor(Color.BLACK);
+            paint.setColor(Color.WHITE);
+            paint.setTextSize(fontSize);
+            if (DEBUGGING) {
+                printDebugginText();
+            }
+            holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    private void printDebugginText() {
+        int debugSize = fontSize;
+        int debugStart = 150;
+        paint.setTextSize(debugSize);
+        canvas.drawText("FPS: " + FPS, 10, debugStart + debugSize, paint);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+    }
+
+    @Override
+    public void run() {
+        while (drawing) {
+            long frameStartTime = System.currentTimeMillis();
+            if (paused) {
+                update();
+            }
+            draw();
+
+            long timeThisFrame = System.currentTimeMillis() - frameStartTime;
+            if (timeThisFrame > 0) {
+                FPS = MILLIS_IN_SECONDS / timeThisFrame;
+            }
+        }
+    }
+
+    private void update() {
+    }
+
+    public void pause() {
+        drawing = false;
+        try {
+            thread.join();
+        } catch (Exception e) {
+            Log.e("Error:","joining thread");
+        }
+    }
+
+    public void resume() {
+        drawing = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+}
--- app/src/main/java/com/example/livedrawing/MainActivity.java
+++ app/src/main/java/com/example/livedrawing/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.livedrawing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
 public class MainActivity extends AppCompatActivity {
+    private LiveDrawingView liveDrawingView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) throws  {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        liveDrawingView = new LiveDrawingView(this, size.x, size.y);
+
+        setContentView(liveDrawingView);
+        String s ;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        liveDrawingView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        liveDrawingView.pause();
     }
 }
